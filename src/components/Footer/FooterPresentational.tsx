7432e279c8de2dbb003b0f96240d74274906fef0
Desolateeeooo <125212823+Desolateeeooo@users.noreply.github.com>
2025-10-10 01:19:21 +0500

    Update Footer component

--- src/components/Footer/FooterPresentational.tsx
+++ src/components/Footer/FooterPresentational.tsx
@@ -1,4 +1,5 @@
 import styles from "@/components/Footer/Footer.module.css"
+import Image from "next/image";
 
 function FooterPresentational() {
 	return (
@@ -7,14 +8,38 @@ function FooterPresentational() {
 				<h2>Связаться со мной</h2>
 				<section className={styles.contact_card_grid_container}>
 					<div className={styles.contact_card}>
-						<div className={styles.text_flex_container}>
-							<p>
-								<span>GitHub:</span> github.com/Desolateeeooo
-							</p>
-						</div>
+						<Image
+							src={"/static/images/icons/icon_9.svg"}
+							alt="GitHub Icon"
+							width={75}
+							height={75}
+						/>
+						<p>
+							<span>GitHub:</span> github.com/Desolateeeooo
+						</p>
 					</div>
-					<div className={styles.contact_card}><p><span>Email:</span> ivan57goridko873@gmail.com</p></div>
-					<div className={styles.contact_card}><p><span>Telegram:</span> @desointj</p></div>
+					<div className={styles.contact_card}>
+						<Image
+							src={"/static/images/icons/gmail_icon.svg"}
+							alt="Gmail Icon"
+							width={75}
+							height={75}
+						/>
+						<p>
+							<span>Email:</span> ivan57goridko873@gmail.com
+						</p>
+					</div>
+					<div className={styles.contact_card}>
+						<Image
+							src={"/static/images/icons/telegram_icon.svg"}
+							alt="Telegram Icon"
+							width={75}
+							height={75}
+						/>
+						<p>
+							<span>Telegram:</span> @desointj
+						</p>
+					</div>
 				</section>
 			</div>
 		</div>
