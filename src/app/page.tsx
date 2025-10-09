1fd58864f86fd7e3dc106e1442a3bca2d1d61ec3
Desolateeeooo <125212823+Desolateeeooo@users.noreply.github.com>
2025-10-10 01:08:22 +0500

    Update Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import NavBarPresentational from "@/components/NavBar/NavBarPresentational";
 import AboutMeContainer from "@/components/AboutMe/AboutMeContainer";
 import MyProjectsPresentational from "@/components/MyProjects/MyProjectsPresentational";
+import FooterPresentational from "@/components/Footer/FooterPresentational";
 
 export default function Home() {
 	return (
@@ -13,7 +14,7 @@ export default function Home() {
 				<MyProjectsPresentational />
 			</main>
 			<footer>
-				
+				<FooterPresentational />
 			</footer>
 		</div>
 	);
--- src/components/Footer/FooterPresentational.tsx
+++ src/components/Footer/FooterPresentational.tsx
@@ -1,3 +1,24 @@
+import styles from "@/components/Footer/Footer.module.css"
+
 function FooterPresentational() {
-	
+	return (
+		<div className={styles.footer_container}>
+			<div className={styles.contact_card_container}>
+				<h2>Связаться со мной</h2>
+				<section className={styles.contact_card_grid_container}>
+					<div className={styles.contact_card}>
+						<div className={styles.text_flex_container}>
+							<p>
+								<span>GitHub:</span> github.com/Desolateeeooo
+							</p>
+						</div>
+					</div>
+					<div className={styles.contact_card}><p><span>Email:</span> ivan57goridko873@gmail.com</p></div>
+					<div className={styles.contact_card}><p><span>Telegram:</span> @desointj</p></div>
+				</section>
+			</div>
+		</div>
+	)
 }
+
+export default FooterPresentational;
